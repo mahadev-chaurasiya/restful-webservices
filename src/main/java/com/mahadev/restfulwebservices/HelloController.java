5622530e59f7ba5ea4786068ed5a5d441688902f
MAHADEV CHAURASIYA <mdchaurasiya@gmail.com>
2023-01-05 22:55:45 +0530

    Bean class added

--- src/main/java/com/mahadev/restfulwebservices/HelloController.java
+++ src/main/java/com/mahadev/restfulwebservices/HelloController.java
@@ -1,6 +1,8 @@
 package com.mahadev.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,17 @@ public class HelloController {
 
 	@GetMapping(path = "hello-world")
 	public String sayHello() {
-		return "Welcome to Springboot";
+		return "Welcome to @GetMapping";
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world2")
+	public String sayHello2() {
+		return "Welcome to @RequestMapping";
+	}
+	
+	@GetMapping(path = "/bean")
+	public HelloWorld helloBean() {
+		return new HelloWorld("Hello world");
+	}
+	
 }
---
+++ src/main/java/com/mahadev/restfulwebservices/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.mahadev.restfulwebservices;
+
+public class HelloWorld {
+	
+	private String name;
+	
+	public HelloWorld(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
